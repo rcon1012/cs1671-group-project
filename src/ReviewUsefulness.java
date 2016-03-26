fc6f4d391ee67f36f9329f6b073ae2a52eff8926
rcon1012 <rcon1012@gmail.com>
2016-03-26 17:00:29 -0400

    word tokenization

--- src/ReviewUsefulness.java
+++ src/ReviewUsefulness.java
@@ -1,4 +1,31 @@
+import java.io.StringReader;
+import java.util.List;
+
 
 public class ReviewUsefulness {
-
+	public static void main(String[] args) {
+		// word tokenization
+		String review = ""; // replace this with review to be tokenized
+		StringReader sr = new StringReader(review);
+		DocumentPreprocessor dp = new DocumentPreprocessor(sr);
+		for (List<HasWord> sentence : dp) {
+			for(int i = 0; i < sentence.size() - 1; i++) {
+				String word = sentence.get(i).word();
+				String nextWord = sentence.get(i + 1).word();
+				// re-concatenate contractions
+	    		if(nextWord.equals("m") || nextWord.equals("ll") || nextWord.equals("d") || 
+	    				nextWord.equals("ve") || nextWord.equals("re") || 
+	    				nextWord.equals("s") || nextWord.equals("ll") || nextWord.equals("nt") ) {
+	    			if(nextWord.equals("nt")) {
+	    				word = word + "n't";
+	    			}
+	    			else {
+	    				word = word + "'" + nextWord;
+	    			}
+	    			i++;
+	    		}
+			}
+		}
+		sr.close();
+	}
 }
